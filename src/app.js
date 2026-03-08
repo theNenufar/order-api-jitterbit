839d4c56741ced2777d92cf0a82dea5e6d9dcd29
theNenufar <nenufarmarinidias@gmail.com>
2026-03-08 01:38:53 -0300

    chore: add order routes and directory structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const orderRoutes = require("./route/order-route")
 const app = express()
 
 app.use(express.json())
+
+app.use("/order", orderRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "API running" })
